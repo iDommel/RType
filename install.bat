fd0205cf834d0b04c052511dc731562a8c598cf7
rox51124 <roxane.baert@epitech.eu>
2022-10-17 18:34:43 +0200

    ADDED: cmake.msi

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-::"TELECHARGE FICHIER"
+::TELECHARGE FICHIER
 @ECHO OFF
 git clone https://github.com/rox51124/File-R-Type.git
 cd File-R-Type
@@ -6,8 +6,11 @@ move conanfile.txt "%~dp0"
 cd ..
 RMDIR /s /q "File-R-Type"
 
-::"INSTALL CONAN"
+::INSTALL CMAKE
+"%~dp0\Executable\cmake.msi"
+
+::INSTALL CONAN
 "%~dp0\Executable\conan.exe"
 
-::"EXECUTE CONAN"
+::EXECUTE CONAN
 conan install conanfile.txt --build=qt -c tools.system.package_manager:mode=install --build=missing
