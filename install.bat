da22a990b8545da1f12c26a28d50b5fae5d43f90
Zac <izaac.carcenac-sautron@epitech.eu>
2022-10-26 18:37:20 +0200

    MODIFY: artifacts moved to build folder

--- install.bat
+++ install.bat
@@ -9,5 +9,5 @@
 
 ::EXECUTE CONAN
 conan install . -s compiler="Visual Studio" -s compiler.version=17 --install-folder cmake-build-release --build=missing -c tools.system.package_manager:mode=install
-cmake . -DCMAKE_TOOLCHAIN_FILE=cmake-build-release/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=cmake-build-release/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build build
