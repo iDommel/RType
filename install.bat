8c17c0c3d12b76e754f2003897a0cb2991f8fa7a
rox51124 <roxane.baert@epitech.eu>
2022-10-26 17:56:03 +0200

    MODIFIED: install.bat

--- install.bat
+++ install.bat
@@ -8,6 +8,6 @@
 "%~dp0\executable\conan.exe"
 
 ::EXECUTE CONAN
-conan install . --install-folder cmake-build-release --build=missing -c tools.system.package_manager:mode=install
+conan install . -s compiler="Visual Studio" -s compiler.version=17 --install-folder cmake-build-release --build=missing -c tools.system.package_manager:mode=install
 cmake . -DCMAKE_TOOLCHAIN_FILE=cmake-build-release/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
