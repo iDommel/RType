8f658585504e0dd7167e8cb70d215807d925ebc4
rox51124 <roxane.baert@epitech.eu>
2022-10-28 23:36:48 +0200

    MODIFIED: cmakelists move output directory

--- install.bat
+++ install.bat
@@ -11,9 +11,3 @@
 conan install . -s compiler="Visual Studio" -s compiler.version=17 --install-folder cmake-build-release --build=missing -c tools.system.package_manager:mode=install
 cmake . -DCMAKE_TOOLCHAIN_FILE=cmake-build-release/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
-
-cd Release
-move r-type_client.exe "%~dp0"
-move r-type_library.lib "%~dp0"
-move r-type_server.exe "%~dp0"
-cd ..
