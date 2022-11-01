41691aa4e34e2242d2ab751c3b6c8b030221b820
Cyril <cyril.dehaese@epitech.eu>
2022-11-01 06:48:28 -0400

    ADDING: trajectory and update in Gamesysteme

--- src/components/Trajectory.cpp
+++ src/components/Trajectory.cpp
@@ -9,12 +9,17 @@
 
 namespace ecs
 {
-    Trajectory::Trajectory()
+    Trajectory::Trajectory(std::function<int(int)> trajFunction, Position pos) : Component(Type::TRAJECTORY), _trajFunction(trajFunction), _origin(pos)
     {
         _isInitialized = true;
+        _timer = 0;
     }
 
-    Trajectory::~Trajectory()
+    // need to add speed of the entity
+    void Trajectory::Update(std::shared_ptr<Position> actualPos)
     {
+        _timer++;
+        actualPos->y = _origin.y + _trajFunction(_timer);
+        actualPos->x = _origin.x + _timer / 60;
     }
 }
--- src/components/Trajectory.hpp
+++ src/components/Trajectory.hpp
@@ -8,6 +8,10 @@
 #ifndef TRAJECTORY_HPP_
 #define TRAJECTORY_HPP_
 
+#include <map>
+#include <functional>
+#include <cmath>
+#include "Position.hpp"
 #include "Component.hpp"
 
 namespace ecs
@@ -15,10 +19,51 @@ namespace ecs
     class Trajectory : public Component
     {
     public:
-        Trajectory();
+        /// @brief List of exemple trjectory functions. Y is the result, X the given value
+        enum class TrajectoryType
+        {
+            /// @brief y = 0
+            LINEAR,
+            /// @brief y = x / tps
+            TOPAFINE,
+            /// @brief y = -x / tps
+            BotAFINE,
+            /// @brief y = log(x / tps)
+            LOG,
+            /// @brief y = -log(x / tps)
+            NEGLOG,
+            /// @brief y = syn(x / tps) * 10
+            SINUSOIDAL,
+            /// @brief y = -syn(x / tps) * 10
+            NEGSINUSOIDAL
+        };
 
-    protected:
+        /// @brief Construct a new trajectory depending a std function
+        /// @param trajectoryFunction that define the trajectory to follow
+        Trajectory(std::function<int(int)> trajFunction, Position pos);
+        /// @brief Called at each update to make the entity move depending to the trajectory
+        void Update(std::shared_ptr<Position> actualPos);
+        /// @brief Use TrajectoryType to call the matching std::function
+        std::map<TrajectoryType, std::function<int(int)>> trajectoryList = {
+            {LINEAR, [](int a)
+             { return 0; }},
+            {TOPAFINE, [](int a)
+             { return a / 60; }},
+            {BOTAFINE, [](int a)
+             { return a / 60 * -1; }},
+            {LOG, [](int a)
+             { return std::log(a / 60); }},
+            {NEGLOG, [](int a)
+             { return std::log(a / 60) * -1; }},
+            {SINUSOIDAL, [](int a)
+             { return std::sin(a / 60) * 10; }},
+            {NEGSINUSOIDAL, [](int a)
+             { return std::sin(a / 60) * -10; }}};
+
     private:
+        std::function<int(int)> _trajFunction;
+        float _timer;
+        Position _origin;
     };
 }
 
